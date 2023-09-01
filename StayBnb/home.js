bc5d851d31769f14fd20aa3e106ec2b2d3b61585
Samuel Emeka <samuel.emeka@kibo.school>
2023-09-01 12:36:04 +0100

    new changes

--- StayBnb/home.js
+++ StayBnb/home.js
@@ -1,4 +1,5 @@
-var navBar = document.getElementById("navBar");
+let navBar = document.getElementById("navBar");
+//let activemenu = document.getElementsByClassName("menu-items");
 
 function togglebtn(){
     navBar.classList.toggle("hidemenu")
